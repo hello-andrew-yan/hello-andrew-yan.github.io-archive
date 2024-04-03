1acfa09225dd0fe26ab27da09c050ae1f2563860
Andrew <hello.andrew.yan@outlook.com>
2024-04-04 00:52:46 +1100

    SVG support and additional animatins.

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,27 +1,11 @@
-import { useState, useEffect } from 'react';
 import './App.css'
 
 import Title from '../title/Title';
 import Navigation from '../navigation/Navigation';
 
 export default function App() {
-    const [isActive, setIsActive] = useState(false);
-    useEffect(() => {
-        const handleScroll = () => {
-            console.log(window.scrollY)
-            if (window.scrollY >= 500) {
-                setIsActive(true);
-            } else {
-                setIsActive(false);
-            }
-        };
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-      }, []);
     return (
-        <div className={isActive ? 'app scroll' : 'app'} >
+        <div className="app" >
             <Navigation/>
             <Title />
             {/* Temporary lmao */}
--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -24,6 +24,17 @@ export default function Title() {
                     <span>ANDREW</span>
                     <span>YAN</span>
                 </h1>
+                <svg
+                    className="stroke"
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlnsXlink="http://www.w3.org/1999/xlink"
+                    viewBox="0 0 83.1 17"
+                    xmlSpace="preserve"
+                >
+                    <path className="path" fill={'#a32f35'} d="M0,15.2c-0.3-8.2,16.1-4.1,21-3.7c7.8,1.1,15.5,2,23.4,2.1c7.1,0.2,36.5-0.7,38.2-8.7C82.6,3,81.1,1.3,79.8,0c1.5,1.3,3.1,2.8,3.2,4.9c-0.8,9.2-31.3,9.8-38.7,9.7c-7.9-0.1-15.7-1.1-23.5-2.1c-3.9-0.5-7.8-1.1-11.6-1.3C5.8,10.8,0.1,10.5,0,15.2L0,15.2z"/>
+                </svg>
+
                 <h2 className="subtitle">
                     <span>Hello</span>
                     , I am a Software Engineer and Front-End Developer living in
