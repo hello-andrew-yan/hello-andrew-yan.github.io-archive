cb05fbe02e04e911d519cb95c21307ff070c76fb
Andrew <hello.andrew.yan@outlook.com>
2024-04-05 01:10:55 +1100

    Subtle changes, commented out SVG support.

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -7,14 +7,14 @@ export default function Title() {
         listItems.forEach((item, index) => {
             setTimeout(() => {
                 item.classList.add("bounce");
-            }, (index + 1) * 500);
+            }, (index + 1) * 200);
         });
 
         listItems = document.querySelectorAll('.subtitle span');
         listItems.forEach((item, index) => {
             setTimeout(() => {
                 item.classList.add("bold");
-            }, (index + 1) * 500);
+            }, (index + 1) * 200);
         });
     }, []);
     return (
@@ -24,7 +24,7 @@ export default function Title() {
                     <span>ANDREW</span>
                     <span>YAN</span>
                 </h1>
-                <svg
+                {/* <svg
                     className="stroke"
                     version="1.1"
                     xmlns="http://www.w3.org/2000/svg"
@@ -32,12 +32,13 @@ export default function Title() {
                     viewBox="0 0 83.1 17"
                     xmlSpace="preserve"
                 >
-                    <path className="path" fill={'#a32f35'} d="M0,15.2c-0.3-8.2,16.1-4.1,21-3.7c7.8,1.1,15.5,2,23.4,2.1c7.1,0.2,36.5-0.7,38.2-8.7C82.6,3,81.1,1.3,79.8,0c1.5,1.3,3.1,2.8,3.2,4.9c-0.8,9.2-31.3,9.8-38.7,9.7c-7.9-0.1-15.7-1.1-23.5-2.1c-3.9-0.5-7.8-1.1-11.6-1.3C5.8,10.8,0.1,10.5,0,15.2L0,15.2z"/>
-                </svg>
+                    <path className="path" fill={'#c4b37f'} d="M0,15.2c-0.3-8.2,16.1-4.1,21-3.7c7.8,1.1,15.5,2,23.4,2.1c7.1,0.2,36.5-0.7,38.2-8.7C82.6,3,81.1,1.3,79.8,0c1.5,1.3,3.1,2.8,3.2,4.9c-0.8,9.2-31.3,9.8-38.7,9.7c-7.9-0.1-15.7-1.1-23.5-2.1c-3.9-0.5-7.8-1.1-11.6-1.3C5.8,10.8,0.1,10.5,0,15.2L0,15.2z"/>
+                </svg> */}
 
                 <h2 className="subtitle">
-                    <span>Hello</span>
-                    , I am a Software Engineer and Front-End Developer living in
+                    <span>Hello</span>, I am a 
+                    <span> Software Enginner</span> and 
+                    <span> Front-End Developer</span> living in
                     <span> Sydney</span>,
                     <span> Australia</span>.
                     Currently studying at <span>UNSW</span>.
